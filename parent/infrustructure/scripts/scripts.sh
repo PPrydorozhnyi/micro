0abeccb9d23d914e573c15b7f5030dad3f86e497
Petro Prydorozhnyi <petro@stakelogic.com>
2021-12-06 18:27:27 +0200

    service discovery

--- parent/infrustructure/scripts/scripts.sh
+++ parent/infrustructure/scripts/scripts.sh
@@ -1,2 +1,9 @@
 kubectl apply -f parent/infrustructure/services/currency-service.yml
 kubectl apply -f parent/infrustructure/deployments/currency-deployment.yml
+
+kubectl scale deployment currency --replicas=0
+
+kubectl apply -f parent/infrustructure/roles/service-discovery-role.yml
+
+kubectl apply clusterrolebinding endpoints-reader-pod --clusterrole=monitoring-endpoints  --serviceaccount=default:default
+
