4998853d1c8eb4f3d9768077ada0d62a01ba486a
Petro Prydorozhnyi <petro@stakelogic.com>
2021-12-06 18:33:34 +0200

    rolebinding

--- parent/infrustructure/scripts/scripts.sh
+++ parent/infrustructure/scripts/scripts.sh
@@ -4,6 +4,4 @@ kubectl apply -f parent/infrustructure/deployments/currency-deployment.yml
 kubectl scale deployment currency --replicas=0
 
 kubectl apply -f parent/infrustructure/roles/service-discovery-role.yml
-
-kubectl apply clusterrolebinding endpoints-reader-pod --clusterrole=monitoring-endpoints  --serviceaccount=default:default
-
+kubectl apply -f parent/infrustructure/roles/service-discovery-rolebinding.yml
